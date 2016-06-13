6bf65004dde2234a83920a67dda40bb409641298
jarredszabadi <jarredszabadi@gmail.com>
2016-06-13 11:14:38 -0400

    adding files

---
+++ RGB/RGB.h
@@ -0,0 +1,19 @@
+//
+//  RGB.h
+//  RGB
+//
+//  Created by Jarred Szabadi on 2016-06-13.
+//  Copyright © 2016 Gymtrack Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RGB.
+FOUNDATION_EXPORT double RGBVersionNumber;
+
+//! Project version string for RGB.
+FOUNDATION_EXPORT const unsigned char RGBVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RGB/PublicHeader.h>
+
+
---
+++ RGBTests/RGBTests.swift
@@ -0,0 +1,28 @@
+//
+//  RGBTests.swift
+//  RGBTests
+//
+//  Created by Jarred Szabadi on 2016-06-13.
+//  Copyright © 2016 Gymtrack Inc. All rights reserved.
+//
+
+import Quick
+import Nimble
+import RGB
+class RGBTests: QuickSpec {
+    override func spec() {
+        describe("RGBUIColor") {
+            it("is a correct representation of the values") {
+                let thoughtbotRed = UIColor(
+                    red: CGFloat(195/255),
+                    green: CGFloat(47/255),
+                    blue: CGFloat(52/255),
+                    alpha: 1
+                )
+                let color = RGBUIColor(red: 195, green: 47, blue: 52)
+                
+                expect(color).to(equal(thoughtbotRed))
+            }
+        }
+    }
+}
